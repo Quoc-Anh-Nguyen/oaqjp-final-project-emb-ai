93d60111bcebd76fc3c5d88ab76007eaef78d27f
Quoc Anh <ngdoquocanh@gmail.com>
2026-07-15 13:42:31 -0400

    task 2

---
+++ emotion_detection.py
@@ -0,0 +1,19 @@
+import requests
+
+EMOTION_PREDICT_URL = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+EMOTION_PREDICT_HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+def run_emotion(text_to_analyze: str):
+    payload = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    response = requests.post(
+        EMOTION_PREDICT_URL,
+        headers=EMOTION_PREDICT_HEADERS,
+        json=payload
+    )
+    response.raise_for_status()
+    return response.json()
